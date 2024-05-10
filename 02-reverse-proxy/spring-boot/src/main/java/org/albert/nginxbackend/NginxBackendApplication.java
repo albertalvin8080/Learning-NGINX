101c162907f26e4c69e375b9fbe7575129cfd83b
AlbertAlvi <albertalvin99@gmail.com>
2024-05-10 13:16:45 -0300

    reverse-proxy

---
+++ 02-reverse-proxy/spring-boot/src/main/java/org/albert/nginxbackend/NginxBackendApplication.java
@@ -0,0 +1,13 @@
+package org.albert.nginxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxBackendApplication.class, args);
+	}
+
+}
---
+++ 02-reverse-proxy/spring-boot/src/test/java/com/albert/nginxbackend/NginxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.nginxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
