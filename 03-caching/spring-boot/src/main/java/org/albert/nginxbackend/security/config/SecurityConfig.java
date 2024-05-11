0de82585f58d224c034017f4a7e3316672fbefdc
AlbertAlvi <albertalvin99@gmail.com>
2024-05-11 20:08:44 -0300

    caching-vary-header

--- 03-caching/spring-boot/src/main/java/org/albert/nginxbackend/security/config/SecurityConfig.java
+++ 03-caching/spring-boot/src/main/java/org/albert/nginxbackend/security/config/SecurityConfig.java
@@ -3,22 +3,45 @@ package org.albert.nginxbackend.security.config;
 import org.albert.nginxbackend.security.filters.VaryHeaderFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig 
-{
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .csrf(c -> c.disable())
-            .authorizeHttpRequests(a -> a.anyRequest().permitAll())
-            .addFilterAfter(new VaryHeaderFilter(), BasicAuthenticationFilter.class);
-            
+                .csrf(c -> c.disable())
+                .formLogin(Customizer.withDefaults())
+                // .cors(c -> c.disable())
+                .authorizeHttpRequests(a -> a.anyRequest().authenticated())
+                .addFilterAfter(new VaryHeaderFilter(), AuthorizationFilter.class);
+
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        var u1 = User.withUsername("albert")
+                .password("1234")
+                .passwordEncoder(pwd -> passwordEncoder().encode(pwd))
+                .build();
+
+        return new InMemoryUserDetailsManager(u1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- 03-caching/spring-boot/src/main/java/org/albert/nginxbackend/security/filters/VaryHeaderFilter.java
+++ 03-caching/spring-boot/src/main/java/org/albert/nginxbackend/security/filters/VaryHeaderFilter.java
@@ -14,9 +14,24 @@ public class VaryHeaderFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        response.setHeader("Vary", "Origin");
-        response.setHeader("Cache-Control", "public, max-age=3600");
-        response.setHeader("Pragma", "public");
+
+        // You could also set these inside each method mapping of your controllers.
+        {
+            // Remember, you may add Vary headers to the response, but you can't seem to be able to remove the default ones added by Spring and sent to Nginx.
+            /**
+             * The value of the header `H-Test:<value>` will be used by Nginx to create a key_caching 
+             * for storing the response from this backend. So if you make a request using `H-Test:12345`,
+             * and intend to use the cached response, the next request must have `H-Test:12345` as a header as well.
+             */
+            // response.setHeader("Vary", "H-Test"); 
+
+            response.setHeader("Cache-Control", "public, max-age=3600");
+            // response.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate"); // Default
+
+            response.setHeader("Pragma", "public");
+            // response.setHeader("Pragma", "no-cache"); // Default
+        }
+
         filterChain.doFilter(request, response);
     }
 
