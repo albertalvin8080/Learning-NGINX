dd69bb2c9893d4220a8b762deb6de8d30a74244e
AlbertAlvi <albertalvin99@gmail.com>
2024-05-14 21:11:39 -0300

    secure-link-md5

---
+++ 08-securing-url/GenerateSecureLink.java
@@ -0,0 +1,34 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+
+public class GenerateSecureLink {
+
+    public static String generateSecureLink(String secret, String uri, long expires)
+            throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String formattedStr = String.format("%s:%s:%d", secret, uri, expires);
+
+        byte[] md5Bytes = MessageDigest.getInstance("MD5").digest(formattedStr.getBytes("UTF-8"));
+        String md5String = Base64.getUrlEncoder().withoutPadding().encodeToString(md5Bytes);
+
+        return String.format("%s?md5=%s&expires=%d", uri, md5String, expires);
+    }
+
+    public static void main(String[] args)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException, ParseException {
+        String secret = "1234567";
+        String uri = "/files/image.png";
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = "2024-10-10";
+        Date date = dateFormat.parse(dateString);
+        long expires = date.getTime() / 1000;
+
+        String secureLink = generateSecureLink(secret, uri, expires);
+        System.out.println(secureLink);
+    }
+}
---
+++ 08-securing-url/secure_link_generator.py
@@ -0,0 +1,26 @@
+import base64
+import hashlib
+import datetime
+import time
+
+def generateSecureLink(secret, uri, expires):
+    # The usage of `:` avoids colisions.
+    formatted_str = f"{secret}:{uri}:{expires}"
+
+    # The hexdigest() method is not used because it returns a string that contains hexadecimal digits. These digits include characters from a to f and 0 to 9. The resulting string may not be safe for use in URLs without further processing. Also, Base64 encoding is more compact than hexadecimal. 
+    md5_bytes = hashlib.md5(formatted_str.encode("utf-8")).digest() # Returns the binary equivalent of the hash value.
+    print(md5_bytes)
+    md5_base64_bytes = base64.urlsafe_b64encode(md5_bytes)
+    print(md5_base64_bytes)
+    md5_string = md5_base64_bytes.decode("utf-8").rstrip("=")
+
+    return f"{uri}?md5={md5_string}&expires={expires}";
+
+if __name__ == "__main__":
+    secret = 1234567 # Must be the same secret used inside `$secure_link_md5`
+    uri = "/files/image.png"
+    date_time = datetime.datetime(2024, 10, 10, 0, 0)
+    expires = int(time.mktime(date_time.timetuple())) # Basically parses the datetime to seconds
+
+    secure_link = generateSecureLink(secret, uri, expires)
+    print(secure_link)
